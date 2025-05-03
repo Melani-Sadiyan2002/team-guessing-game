93fa83f5463a7cea7a818bd6c9c21ff369f4aae3
Praveena Wijerathna <praveenawijerathne1@gmail.com>
2025-05-03 10:20:37 +0530

    Added replay feature

--- guessing_game.py
+++ guessing_game.py
@@ -1,10 +1,15 @@
 import random
 
-number = random.randint(1, 10)
-print("Guess a number between 1 and 10")
-guess = int(input())
+while True:
+    number = random.randint(1, 100)
+    print("Guess a number between 1 and 100")
+    guess = int(input())
 
-if guess == number:
-    print("You win!")
-else:
-    print(f"Wrong! The number was {number}")
+    if guess == number:
+        print("You win!")
+    else:
+        print(f"Wrong! The number was {number}")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != 'y':
+        break
